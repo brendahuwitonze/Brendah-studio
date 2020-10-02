5478fffbddfb4decffc184bf6fcd2fb87f78f487
uwitonze brendah <uwitonzebrendah@gmail.com>
2020-10-02 13:51:17 +0200

    my fisrt commit

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    // $('.design').click(function(){
+    //     $('#des').toggle();
+    // });
+    // $('.dev').click(function(){
+    // $('#de').toggle().fadeIn("slow")
+    // })
+
+});
